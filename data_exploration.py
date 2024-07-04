969286a991bdf68ce6e8beb97316a9a3881e8c0f
mayamz <mayametz@gmail.com>
2024-07-04 11:00:25 +0300

    added loading df

--- data_exploration.py
+++ data_exploration.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+DATA_PATH = "./data/"
+
+metadata = pd.read_csv(f"{DATA_PATH}train_metadata.csv")
+data = pd.read_csv(f"{DATA_PATH}train_data.csv")
